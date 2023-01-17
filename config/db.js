db8eac66f3f54ee13c81838f083be6274574caf3
Jose <jruizgall@uoc.edu>
2023-01-17 19:46:57 +0100

    error6 en sequelize

--- config/db.js
+++ config/db.js
@@ -18,6 +18,8 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
         idle: 10000
     },
     operatorAliases: false
-});
+    });
+   
+    export const DB_PORT = process.env.DB_PORT || 3308
 
 export default db;
