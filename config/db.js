b22ad4e12888b10ec4c6c6e4d6cb67747c14c354
Jose <jruizgall@uoc.edu>
2023-01-17 18:21:04 +0100

    error3 en sequelize

--- config/db.js
+++ config/db.js
@@ -8,10 +8,8 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     port: '3308',
     dialect: 'mysql',
     define: {
-        paranoid: true,
-      timestamp: true,
-      freezeTableName: true,
-      underscored: false
+        timestamps: false,
+     
     },
     pool: {
         max: 5,
