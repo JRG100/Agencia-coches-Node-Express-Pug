35727d36d9144d2d5e1811d9f5b73e0227752156
Jose <jruizgall@uoc.edu>
2023-01-17 18:13:25 +0100

    error2 en sequelize

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv/config';
 
 console.log(process.env.DB_HOST);
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, process.env.DB_PORT,  {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: '3308',
-    dialect: 'postgres',
+    dialect: 'mysql',
     define: {
         paranoid: true,
       timestamp: true,
