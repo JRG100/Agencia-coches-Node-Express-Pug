3756dfa64e33f6168d727d9fe8c702bdee1a3d29
Jose <jruizgall@uoc.edu>
2023-01-17 19:50:21 +0100

    error7 en sequelize

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ console.log(process.env.DB_HOST);
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: '3308',
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false,
@@ -20,6 +20,6 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     operatorAliases: false
     });
    
-    export const DB_PORT = process.env.DB_PORT || 3308
+    
 
 export default db;
